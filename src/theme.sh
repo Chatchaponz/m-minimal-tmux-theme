db4cfbb9ece181fad3ccd9e178c6b479458f04e0
chatchapon <chatchapon.ryu@gmail.com>
2025-01-31 22:51:30 +0700

    add mono minimal theme

---
+++ src/theme.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export LC_ALL=en_US.UTF-8
+
+# Set the maximum length of the left component of the status line. The default is 10.
+tmux set-option -g status-left-length 100
+# Set the maximum length of the right component of the status line. The default is 40.
+tmux set-option -g status-right-length 100
+
+tmux set-window-option -g window-status-activity-style "italics"
+tmux set-window-option -g window-status-bell-style "bold"
+
+# message styling
+tmux set-option -g message-style "bg=blue,fg=black,bold"
+
+# pane border color
+tmux set-option -g pane-active-border-style "fg=blue"
+tmux set-option -g pane-border-style "#{?pane_synchronized,fg=blue,fg=brightblack}"
+
+# status bar
+tmux set-option -g status-style "bg=default,fg=white"
+
+### Left side
+tmux set-option -g status-left "#[fg=black,bold]#{?client_prefix,#[bg=yellow],#[bg=blue]}   #S #[bg=default]#{?client_prefix,#[fg=yellow],#[fg=blue]} "
+# tmux set-option -g status-left "#[bg=default,bold]#{?client_prefix,#[fg=yellow],#[fg=blue]}   #S    "
+
+### Windows list
+tmux set-window-option -g window-status-format " #[bg=default,fg=white]#I #{?window_zoomed_flag, , }#W"
+tmux set-window-option -g window-status-current-format "#[bg=default,fg=blue] #I #{?window_zoomed_flag, , }#W"
+
+### Right side
+tmux set-option -g status-right " 󰃰  %b %e %Y %H:%M "
