1bd1c59cc7c49f1e5ff6fbf8323d003e76dc3fa1
chatchapon <chatchapon.ryu@gmail.com>
2025-02-02 21:45:50 +0700

    add support for last window and bell flag

--- src/theme.sh
+++ src/theme.sh
@@ -8,9 +8,6 @@ tmux set-option -g status-left-length 100
 # Set the maximum length of the right component of the status line. The default is 40.
 tmux set-option -g status-right-length 100
 
-tmux set-window-option -g window-status-activity-style "italics"
-tmux set-window-option -g window-status-bell-style "bold"
-
 # message styling
 tmux set-option -g message-style "bg=blue,fg=black,bold"
 
@@ -22,14 +19,14 @@ tmux set-option -g pane-border-style "#{?pane_synchronized,fg=blue,fg=brightblac
 tmux set-option -g status-style "bg=default,fg=white"
 
 ### Left side
-tmux set-option -g status-left "#[fg=black,bold]#{?client_prefix,#[bg=yellow],#[bg=blue]}   #S #[bg=default]#{?client_prefix,#[fg=yellow],#[fg=blue]}  "
+tmux set-option -g status-left "#[fg=black,bold]#{?client_prefix,#[bg=yellow],#[bg=brightblue]}   #S #[bg=default]#{?client_prefix,#[fg=yellow],#[fg=brightblue]}  "
 # tmux set-option -g status-left "#[bg=default,bold]#{?client_prefix,#[fg=yellow],#[fg=blue]}   #S    "
 
 ### Windows list
 # tmux set-window-option -g window-status-format " #[bg=default,fg=white]#{?window_last_flag, , }#I #{?window_zoomed_flag, , }#W"
 # tmux set-window-option -g window-status-current-format "#[bg=default,fg=blue]  #I #{?window_zoomed_flag, , }#W"
-tmux set-window-option -g window-status-format "#[bg=default,fg=white]#I #{?window_zoomed_flag, , }#W"
-tmux set-window-option -g window-status-current-format "#[bg=default,fg=blue]#I #{?window_zoomed_flag, , }#W"
+tmux set-window-option -g window-status-format "#[bg=default]#{?window_bell_flag,#[fg=red],#[fg=white]}#I#{?window_last_flag,∙, }#{?window_zoomed_flag, ,#{?window_bell_flag, , }}#W"
+tmux set-window-option -g window-status-current-format "#[bg=default,fg=blue]#I∙#{?window_zoomed_flag, , }#W"
 tmux set-window-option -g window-status-separator "  "
 
 ### Right side
